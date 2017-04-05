e35c4df6963bf5c6f8fc0b5b05e9a884e4886b04
rgargente <rgargente@gmail.com>
2017-04-05 19:38:16 +0100

    sample user interaction

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import kivy
 from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
 kivy.require('1.9.1')  # replace with your current kivy version !
 
 
 class WaypointerWidget(BoxLayout):
-    pass
+    result_lbl = ObjectProperty(None)
+    waypoint_had = ObjectProperty(None)
+    position_had = ObjectProperty(None)
+
+    def guideme_click(self):
+        text = self.waypoint_had.heading
+        self.result_lbl.text = text
 
 
 class WaypointerApp(App):
